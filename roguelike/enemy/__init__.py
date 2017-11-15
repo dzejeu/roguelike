4e17b70f2217d1f091fec29670152b9291a6a944
dzejeu <michal.swat94@gmail.com>
2017-11-15 22:25:33 +0100

    working enemy

--- roguelike/enemy/__init__.py
+++ roguelike/enemy/__init__.py
@@ -2,7 +2,5 @@ from roguelike.model.character import Character
 
 class BaseEnemy(Character):
 
-    def find_best_direction_to_move(self, target_tile):
+    def find_best_tile_to_move(self, target_tile):
         raise NotImplemented()
-
-
