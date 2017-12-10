247b8369ac38f69dc377f5a1c41c5377d89c28ec
Mark <m.szczotka@hotmail.com>
2017-12-10 23:03:23 +0100

    set max_hp after completed level

--- roguelike/model/world/skill.py
+++ roguelike/model/world/skill.py
@@ -9,3 +9,4 @@ class Skill:
         self.Player.max_hp = (log2(self.Player.max_hp)*0.4*level)+self.Player.max_hp
         self.Player.base_attack = (log2(self.Player.base_attack)*0.4*level)+self.Player.base_attack
         self.Player.base_defense = (log2(self.Player.base_defense)*0.4*level)+self.Player.base_defense
+        self.Player.hp = self.Player.max_hp
